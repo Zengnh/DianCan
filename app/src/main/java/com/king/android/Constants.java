79c1e9f19501bff5826bdd9140cb53c4c9d1f1e4
Zengnh <121632940@qq.com>
2022-11-09 16:25:38 +0800

    ipconfig

--- app/src/main/java/com/king/android/Constants.java
+++ app/src/main/java/com/king/android/Constants.java
@@ -4,8 +4,8 @@ public
 interface Constants {
 
 
-    String HOST = "http://hualien.yizhifang.shop";
-//    String HOST = "http://192.168.2.193:8082";
+
+    String HOST = "http://192.168.2.193:8082";
 
     int pageSize = 20;
 }
