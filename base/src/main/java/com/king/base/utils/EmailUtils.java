acbb822777c20be87b7df5351d8ac1735d6d20bb
Zengnh <121632940@qq.com>
2022-11-09 15:48:34 +0800

    重新处理

--- base/src/main/java/com/king/base/utils/EmailUtils.java
+++ base/src/main/java/com/king/base/utils/EmailUtils.java
@@ -1,7 +1,7 @@
 package com.king.base.utils;
 
-import com.xiang.mail.Mail;
-import com.xiang.mail.MailSender;
+//import com.xiang.mail.Mail;
+//import com.xiang.mail.MailSender;
 
 public
 class EmailUtils {
@@ -10,17 +10,18 @@ class EmailUtils {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Mail mail = new Mail();
-                mail.setMailServerHost("smtp.qq.com");
-                mail.setMailServerPort("25");
-                mail.setUserName("2149962127");
-                mail.setPassword("ijtwskrvumskdjdb");
-                mail.setFromAddress("2149962127@qq.com");
-                mail.setToAddress(new String[]{"784065153@qq.com"});
-                mail.setSubject(subject);
-                mail.setContent(content);
-                MailSender mailSender = new MailSender();
-                mailSender.sendTextMail(mail);
+                //    implementation 'com.github.zongxiaomi:AndroidMail:1.07'
+//                Mail mail = new Mail();
+//                mail.setMailServerHost("smtp.qq.com");
+//                mail.setMailServerPort("25");
+//                mail.setUserName("2149962127");
+//                mail.setPassword("ijtwskrvumskdjdb");
+//                mail.setFromAddress("2149962127@qq.com");
+//                mail.setToAddress(new String[]{"784065153@qq.com"});
+//                mail.setSubject(subject);
+//                mail.setContent(content);
+//                MailSender mailSender = new MailSender();
+//                mailSender.sendTextMail(mail);
             }
         }).start();
     }
