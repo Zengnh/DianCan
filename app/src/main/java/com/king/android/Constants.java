696cc2804fb0e6f7bd6a924d647efb2872bac8a4
Zengnh <121632940@qq.com>
2022-11-09 15:53:10 +0800

    change config

--- app/src/main/java/com/king/android/Constants.java
+++ app/src/main/java/com/king/android/Constants.java
@@ -3,8 +3,8 @@ package com.king.android;
 public
 interface Constants {
 
+
     String HOST = "http://hualien.yizhifang.shop";
-//    String HOST = "http://w0039.okapp.store";
 //    String HOST = "http://192.168.2.193:8082";
 
     int pageSize = 20;
